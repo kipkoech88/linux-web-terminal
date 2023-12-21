5d57cecbba920e29ac222f7090b529343a871baf
kipkoech88 <hoseakipkoech88@gmail.com>
2023-12-22 00:50:32 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
+const {spawn} = require('child_process')
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
+
+//middlewares 
+app.use(cors())
+
+
+app.get('/', (req, res)=>{
+    res.send('Web based linux terminal')
+})
+
+io.on('connection', (socket)=>{
+    console.log('User connected')
+
+    //handle socketio events
+
+    socket.on('execute', (command)=>{
+        const childProcess = spawn(command, {shell: true})
+
+        childProcess.stdout.on('data', (data)=>{
+            socket.emit(`output`, data.toString())
+        })
+
+        childProcess.on(`disconnect`, (code)=>{
+            socket.emit(`output`, `process exited with code ${code}`)
+        })
+    })
+
+    socket.on('disconnect', ()=>{
+        console.log(`User disconnected`)
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
